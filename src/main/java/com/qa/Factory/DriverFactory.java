efcdd2c7c32ff53fc425b2b8d839d04759bd72b3
AbhiPatil9496 <140178042+AbhiPatil9496@users.noreply.github.com>
2023-07-25 16:16:31 +0530

    commit

---
+++ src/main/java/com/qa/Factory/DriverFactory.java
@@ -0,0 +1,13 @@
+package com.qa.Factory;
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.OptionsManager;
+
+public class DriverFactory {
+	WebDriver driver;
+	OptionsManager optionsManager;
+	public static Properties prop;
+}
---
+++ src/main/java/com/qa/Factory/OptionsManager.java
@@ -0,0 +1,14 @@
+package com.qa.Factory;
+
+import java.util.Properties;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class OptionsManager {
+	private Properties prop;
+	private ChromeOptions co;
+	private FirefoxOptions fo;
+	private EdgeOptions eo;
+}
